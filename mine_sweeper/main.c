2abe801ac072d5fc80f9f47ccf0ac61049e7ae88
Nico Vallone <nichov@campus-010-073.ucdavis.edu>
2017-06-08 19:58:20 -0700

    Initial Commit

---
+++ mine_sweeper/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mine_sweeper
+//
+//  Created by Nico Vallone on 8/6/2017.
+//  Copyright © 2017 Nico Vallone. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
